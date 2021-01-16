4df5b2ab4a0fa8d020b18537f5de77470672a573
Angel Gladin <angelgladin@gmail.com>
2021-01-16 14:42:39 -0600

    Add dependencies for parsing the json, create a dummy readme file, add transactions.json, complete reading a json from local

--- cli-report-generator/build.gradle.kts
+++ cli-report-generator/build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.21"
+    kotlin("kapt") version "1.4.21"
     application
 }
 
@@ -13,9 +14,11 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test-junit5"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+    implementation("com.squareup.moshi:moshi:1.11.0")
+    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.11.0")
+    //testImplementation(kotlin("test-junit5"))
+    //testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    //testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
 }
 
 tasks.test {
--- cli-report-generator/src/main/kotlin/main.kt
+++ cli-report-generator/src/main/kotlin/main.kt
@@ -1,3 +1,23 @@
+import com.squareup.moshi.JsonClass
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+import java.io.File
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    // Root path of the json
+    val fileName = "src/main/resources/transactions.json"
+    // Since it's a "small" file, we can read it all and put it in a String
+    val content = File(fileName).readText()
+
+    // Parsing the json
+    val moshi = Moshi.Builder().build()
+    val type = Types.newParameterizedType(MutableList::class.java, Transaction::class.java)
+    val jsonAdapter = moshi.adapter<List<Transaction>>(type)
+    val transactions = jsonAdapter.fromJson(content)
+    println(transactions)
 }
+
+@JsonClass(generateAdapter = true)
+data class Transaction(
+    val uuid: String
+)
